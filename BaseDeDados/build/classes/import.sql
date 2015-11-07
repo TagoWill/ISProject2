fdfb78ad735c8de6b38fac2170fb4adedc98e9cf
TagoWill <tagowill@gmail.com>
2015-11-07 01:28:09 +0000

    AutoPreencher tabelas a funcionar

---
+++ BaseDeDados/build/classes/import.sql
@@ -0,0 +1,3 @@
+#import file for database initialization
+
+Insert into users (id,email,nome,password,user) value(1,"tagowill@gmail.com","Tiago", "13", "tago");
--- BaseDeDados/src/fillup.sql
+++
@@ -1,4 +0,0 @@
-#import file for database initialization
-
-Insert into users (email,nome,password,user) value("tagowill@gmail.com","Tiago", "13", "tago");
-
